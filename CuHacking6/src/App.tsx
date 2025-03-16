fe89b3849d182329dafaa7d888ef32d66fd58d1c
kinhvin <kevintruong.code@gmail.com>
2025-03-16 03:15:32 -0400

    updated homepage link hover

--- CuHacking6/src/App.tsx
+++ CuHacking6/src/App.tsx
@@ -2,14 +2,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LaunchPage from './LaunchPage';
-import Stats from './Stats';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LaunchPage />} />
-        <Route path="/stats" element={<Stats />} />
       </Routes>
     </BrowserRouter>
   );
